dc90cf5e5e0e8f3d9722a29a9f9a83d2cbe7d719
github username <github email address>
2016-05-28 19:57:00 -0400

    modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :users
 
   root 'welcome#index'
 
@@ -13,7 +14,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
-  resources :users
 
 
 
