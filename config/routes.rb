3426f94df563f680e5b220c1b404ba5366c3bbda
github username <github email address>
2016-05-28 18:31:04 -0400

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
-    delete '/logout' => 'sessions#destroy', as: "logout"
-
+    delete 'logout', to: 'sessions#destroy'
+    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
