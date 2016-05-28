2f0d524d2f926a91e1d36277dd7b170cfe99394d
github username <github email address>
2016-05-28 19:33:26 -0400

    modified:   app/controllers/sessions_controller.rb new file:   app/views/sessions/logout.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     flash[:success] = "You have logged out"
     redirect_to root_path
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
